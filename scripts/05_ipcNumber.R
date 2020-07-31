57b89e76c9f79fa0932d2ad50dcbea2321c2b737
seheeopark <54168331+seheeopark@users.noreply.github.com>
2020-07-31 20:13:16 +0900

    minor corrections

--- scripts/05_ipcNumber.R
+++ scripts/05_ipcNumber.R
@@ -87,10 +87,14 @@ get_full_patents <- function(x) {
   return (cumul_df)
   if (temp$status_code != 200) {
     print(paste0("Error occurred! Inspect the API status code:", temp$status_code))
-    } 
+  } 
+  if (length(cumul_df) == 0) {
+    resultMsg <- temp %>% read_xml() %>% xml_find_all(xpath = "//resultMsg") %>% xml_text() 
+    # No message if there is no error 
+    print(paste0("API access result: ", resultMsg))
+  }
 } 
 
-# (2)  Get real patent data from a vector of applicant names, and multiple pages 
-# use your ipc vector within seq_along() 
+# (2)  Get real patent data from a vector of ipc numbers 
 df_full_patents <- map_dfr(seq_along(ipc_sample), get_full_patents)
 
